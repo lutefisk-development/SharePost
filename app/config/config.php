c850172d597df45420b5b797ab6125e5910523b4
Per Kristian <kristiansvanberg@gmail.com>
2019-02-14 12:53:25 +0100

    first commit of the project

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'mysql');
+  define('DB_NAME', 'tmvc');
+
+  // App root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URL root
+  define('URLROOT', 'http://localhost/_Udemy/OOP_PHP/traversymvc');
+
+  // Site name
+  define('SITENAME','Complete MVC');
